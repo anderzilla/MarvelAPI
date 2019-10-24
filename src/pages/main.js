f61b2f8305cb15480c855758a1cf2aa5c10309c6
Anderson <anderson.hg25@gmail.com>
2019-10-23 23:34:57 -0300

    Ajustes de layout

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import api from '../services/api';
-import md5 from 'react-native-md5';
-import moment from 'moment';
+import { TouchableOpacity, StyleSheet, View, FlatList, Text, Image, TextInput } from 'react-native'
+import md5 from 'js-md5'
+
+const publicKey = 'e39129ffea4355dfde00740abcb1766b';
+const privatKey = 'ff64cfb9de7914cd99ec63b8f4c2873ad6ee8f36';
 
 export default class Main extends Component{
+    state = {
+        text: '',
+    }
+    
     static navigationOptions = {
-        title: 'MarvelSearch',
-        headerStyle: {backgroundColor: '#d60000'},
-        headerTintColor: '#fff',
-        headerTitleStyle: {fontWeight: 'bold'}
+        title: "BUSCA MARVEL TESTE FRONTEND",
+        headerStyle: {
+            backgroundColor: '#FFF',
+          },
+          headerTintColor: '#D90000',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
     };
 
     componentDidMount() {
@@ -17,8 +26,7 @@ export default class Main extends Component{
     }
     
     loadCharacters = async() => {
-        const publicKey = 'e39129ffea4355dfde00740abcb1766b';
-        const privatKey = 'ff64cfb9de7914cd99ec63b8f4c2873ad6ee8f36';
+        
         const ts = moment();
         const hash = md5.str_md5(ts+privatKey+publicKey);
         const res = await api.get('/v1/public/characters?ts=1&apikey=e39129ffea4355dfde00740abcb1766b&hash=5c7ad4393c645610c67a5bf17efcf0d8');
@@ -28,9 +36,74 @@ export default class Main extends Component{
 
     render(){
         return(
-            <View>
-                <Text>Main Page {this.hash}</Text>
+            <View style={styles.container}>
+                <Text style={styles.label}>
+                NOME DO PERSONAGEM
+                </Text>
+                <TextInput
+                style={{ 
+                    borderRadius:5, 
+                    borderColor:"#cecece", 
+                    height: 40, 
+                    borderWidth: 1, 
+                    width: 310,
+                    marginLeft:30,
+                    color: "#909090"
+                }}
+                placeholder="Type here to translate!"
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+                />
+                <View style={styles.barraTituloLista}>
+                    <Text style={styles.tituloBarra}>Nome</Text>
+                </View>
+                <FlatList
+                data={[
+                    {key: 'Devin'},
+                    {key: 'Dan'},
+                    {key: 'Dominic'},
+                    {key: 'Jackson'},
+                    {key: 'James'},
+                    {key: 'Joel'},
+                    {key: 'John'},
+                    {key: 'Jillian'},
+                    {key: 'Jimmy'},
+                    {key: 'Julie'},
+                ]}
+                renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                />
             </View>    
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     justifyContent: "center",
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+    label: {
+        fontSize:10,
+        marginLeft:30,
+        marginTop: 12,
+        color: "#d90000",
+    },
+    barraTituloLista:{
+        backgroundColor: "#d90000",
+        color: "#ffffff",
+        marginTop: 12,
+        marginBottom: 12
+    },
+    tituloBarra: {
+        color: "#ffffff",
+        fontSize: 16,
+        lineHeight: 40,
+        marginLeft: 120
+    }
+
+  })
